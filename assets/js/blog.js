9149bf3f531b93a9ccfe27ca3a04803ddeeb2079
khardigree <kaitlynnhardigree@gmail.com>
2024-05-01 00:14:29 -0400

    finalized HTML, created JavaScript for back button and both toggles, updated CSS for dark-mode

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,13 +1,62 @@
-
 const storedFormData = JSON.parse(localStorage.getItem('blogPosts')) || [];
+const postContainer = document.getElementById("post");
 
 console.log(storedFormData);
 
+// Create a new form data object
+let newFormData = {
+    username: $("#username").val(),
+    title: $("#title").val(),
+    post: $("#post").val(),
+};
 
-    // storedFormData.push(formData);
+const user = storedFormData.username;
+const title = storedFormData.title;
+const post = storedFormData.post;
 
-    // localStorage.setItem('formData', JSON.stringify(storedFormData))
+const blogPost ={
+    user, 
+    title, 
+    post,
+}
 
-    // blogPost.push(blogPost);
+postContainer.textContent = `User: ${blogPost.user}, Title: ${blogPost.title}, Post: ${blogPost.post}`;
 
-    // let blogPost = localStorage.getItem('blogPosts');
+function saveFormData(formData) {
+    storedFormData.push(formData);
+    if (Array.isArray(storedFormData)) {
+        storedFormData.forEach(data => {
+            let newPost = document.createElement("div");
+            newPost.textContent = `User: ${data.username}, Title: ${data.title}, Post: ${data.post}`;
+            postContainer.appendChild(newPost);
+        });
+    }
+}
+
+
+
+// if (Array.isArray(storedFormData)) {
+//     storedFormData.forEach(data => {
+//         let newPost = document.createElement("div");
+//         newPost.textContent = `User: ${data.username}, Title: ${data.title}, Post: ${data.post}`;
+//         postContainer.appendChild(newPost);
+//     });
+// }
+
+
+//add eventListener for toggle
+function darkMode() {
+    event.preventDefault();
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+   
+}
+document.getElementById("darkModeButton").addEventListener("click", darkMode);
+
+//add eventListener for back button
+function goBack() {
+    event.preventDefault();
+    const element = document.body;
+    window.location.href = "index.html";
+}
+document.getElementById("back").addEventListener("click", goBack);
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,10 @@
 //darkmode toggle
+function darkMode() {
+    event.preventDefault();
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+   
+}
+document.getElementById("darkModeButton").addEventListener("click", darkMode);
+
 
